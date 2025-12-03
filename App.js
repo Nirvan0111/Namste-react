18fda2b75e203eaf26af7fbbaf627db8b042536b
Macbook <macbook@Macbooks-MacBook-Pro.local>
2025-12-03 15:40:46 +0530

    Initial steps for learning React-js

---
+++ App.js
@@ -0,0 +1,7 @@
+const heading = React.createElement(
+  "h2",
+  { id: "heading" },
+  "hello nirvan from react js "
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
